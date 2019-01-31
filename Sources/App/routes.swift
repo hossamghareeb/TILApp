57877023215ca26a940cbc4ec745fb8d82e070ba
Hossam Ghareeb <hossam.ghareb@gmail.com>
2019-01-31 23:10:51 +0400

    Support basic CRUD operations

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,9 @@
 import Vapor
 
+struct AcronymsResponse: Content {
+    let data: [Acronym]
+}
+
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
@@ -12,7 +16,32 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
     
+    /// Create
     router.post(Acronym.self, at: "api", "acronyms") { request, acronym -> Future<Acronym> in
         return acronym.save(on: request)
+    }
+    
+    /// GET all
+    router.get("api", "acronyms") { request -> Future<AcronymsResponse> in
+        return Acronym.query(on: request).all().map { AcronymsResponse(data: $0) }
+    }
+    
+    /// GET by ID
+    router.get("api", "acronyms", Acronym.parameter) { request -> Future<Acronym> in
+        return try request.parameters.next(Acronym.self)
+    }
+    
+    /// Update
+    router.put(Acronym.self, at: "api", "acronyms", Acronym.parameter) { request, newAcronym -> Future<Acronym> in
+        try request.parameters.next(Acronym.self).flatMap(to: Acronym.self, { oldAcronym in
+            oldAcronym.short = newAcronym.short
+            oldAcronym.long = newAcronym.long
+            return oldAcronym.save(on: request)
+        })
+    }
+    
+    /// Delete by ID
+    router.delete("api", "acronyms", Acronym.parameter) { request -> Future<HTTPStatus> in
+        try request.parameters.next(Acronym.self).delete(on: request).transform(to: HTTPStatus.noContent)
     }
 }
